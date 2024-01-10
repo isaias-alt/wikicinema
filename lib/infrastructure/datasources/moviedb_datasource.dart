270d954f4ec8e936851c7c00e77eea8022e28daa
isaias-alt <cascolucasisaias@gmail.com>
2024-01-09 22:36:29 -0300

    refactor(infrastructure): map response in MovieDB datasource

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -2,6 +2,8 @@ import 'package:dio/dio.dart';
 import 'package:wikicinema/config/constants/environment.dart';
 import 'package:wikicinema/domain/datasources/movies_datasource.dart';
 import 'package:wikicinema/domain/entities/movie.dart';
+import 'package:wikicinema/infrastructure/mappers/movie_mapper.dart';
+import 'package:wikicinema/infrastructure/models/moviedb/moviedb_response.dart';
 
 class MoviedbDatasource extends MoviesDatasource {
   final dio = Dio(
@@ -17,7 +19,11 @@ class MoviedbDatasource extends MoviesDatasource {
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
     final response = await dio.get('/movie/now_playing');
-    final List<Movie> movies = [];
+    final movieDBResponse = MovieDbResponse.fromJson(response.data);
+    final List<Movie> movies = movieDBResponse.results
+        .where((moviedb) => moviedb.posterPath != 'no-poster')
+        .map((moviedb) => MovieMapper.movieDBToEntity(moviedb))
+        .toList();
     return movies;
   }
 }
