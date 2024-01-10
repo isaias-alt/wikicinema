feee8a90874a8b9915e9b30a2954585f06cafa57
isaias-alt <cascolucasisaias@gmail.com>
2024-01-09 22:46:14 -0300

    feat(infrastructure): implement repository

---
+++ lib/infrastructure/repositories/movie_repository_impl.dart
@@ -0,0 +1,13 @@
+import 'package:wikicinema/domain/entities/movie.dart';
+import 'package:wikicinema/domain/datasources/movies_datasource.dart';
+import 'package:wikicinema/domain/repositories/movies_repository.dart';
+
+class MovieRepositoryImpl extends MoviesRepository {
+  final MoviesDatasource datasource;
+
+  MovieRepositoryImpl(this.datasource);
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) {
+    return datasource.getNowPlaying(page: page);
+  }
+}
