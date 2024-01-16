7e494d4bf84e064b6d709d673df231769a3ed9b6
isaias-alt <cascolucasisaias@gmail.com>
2024-01-16 18:19:02 -0300

    refactor(infrastructure): add required parameter

--- lib/infrastructure/repositories/movie_repository_impl.dart
+++ lib/infrastructure/repositories/movie_repository_impl.dart
@@ -5,7 +5,7 @@ import 'package:wikicinema/domain/repositories/movies_repository.dart';
 class MovieRepositoryImpl extends MoviesRepository {
   final MoviesDatasource datasource;
 
-  MovieRepositoryImpl(this.datasource);
+  MovieRepositoryImpl({required this.datasource});
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) {
     return datasource.getNowPlaying(page: page);
--- lib/presentation/providers/movies/movies_repository_provider.dart
+++ lib/presentation/providers/movies/movies_repository_provider.dart
@@ -3,5 +3,5 @@ import 'package:wikicinema/infrastructure/datasources/moviedb_datasource.dart';
 import 'package:wikicinema/infrastructure/repositories/movie_repository_impl.dart';
 
 final movieRepositoryProvider = Provider<MovieRepositoryImpl>((ref) {
-  return MovieRepositoryImpl(MoviedbDatasource());
+  return MovieRepositoryImpl(datasource: MoviedbDatasource());
 });
