8d4a825e09d8433092fef753a9a428999dc4a149
isaias-alt <cascolucasisaias@gmail.com>
2024-02-19 20:39:21 -0300

    feat(presentation): add provider

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:wikicinema/domain/entities/movie.dart';
+import 'package:wikicinema/presentation/providers/providers.dart';
 
-class MovieScreen extends StatefulWidget {
+class MovieScreen extends ConsumerStatefulWidget {
   static const name = 'movie-screen';
 
   final String movieId;
@@ -11,17 +14,27 @@ class MovieScreen extends StatefulWidget {
   });
 
   @override
-  State<MovieScreen> createState() => _MovieScreenState();
+  MovieScreenState createState() => MovieScreenState();
 }
 
-class _MovieScreenState extends State<MovieScreen> {
+class MovieScreenState extends ConsumerState<MovieScreen> {
   @override
   void initState() {
     super.initState();
+    ref.read(movieInfoProvider.notifier).getMovie(widget.movieId);
   }
 
   @override
   Widget build(BuildContext context) {
+    final Movie? movie = ref.watch(movieInfoProvider)[widget.movieId];
+    if (movie != null) {
+      return const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Movie ID: ${widget.movieId}'),
