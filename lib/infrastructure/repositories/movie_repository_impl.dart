491f7093c1c534454d59b3ccae22d1d19e0fc30b
isaias-alt <cascolucasisaias@gmail.com>
2024-02-20 16:08:17 -0300

    feat(infrastructure): add get movie by id

--- lib/infrastructure/repositories/movie_repository_impl.dart
+++ lib/infrastructure/repositories/movie_repository_impl.dart
@@ -6,6 +6,7 @@ class MovieRepositoryImpl extends MoviesRepository {
   final MoviesDatasource datasource;
 
   MovieRepositoryImpl({required this.datasource});
+
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) {
     return datasource.getNowPlaying(page: page);
