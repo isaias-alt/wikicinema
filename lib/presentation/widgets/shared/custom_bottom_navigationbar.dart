7aeb4681411d62a7e60a0b8f024c1a22f9478828
isaias-alt <cascolucasisaias@gmail.com>
2024-02-25 23:29:56 -0300

    feat(presentation): add go router oficial solutio implemented

--- lib/presentation/widgets/shared/custom_bottom_navigationbar.dart
+++ lib/presentation/widgets/shared/custom_bottom_navigationbar.dart
@@ -1,12 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomBottomNavigationbar extends StatelessWidget {
   const CustomBottomNavigationbar({super.key});
 
+  static int getCurrentIndex(BuildContext context) {
+    final location = GoRouterState.of(context).fullPath;
+
+    switch (location) {
+      case '/':
+        return 0;
+
+      case '/categories':
+        return 1;
+
+      case '/favorites':
+        return 2;
+
+      default:
+        return 0;
+    }
+  }
+
+  void onItemTapped(BuildContext context, int value) {
+    switch (value) {
+      case 0:
+        context.go('/');
+        break;
+      case 1:
+        context.go('/');
+        break;
+      case 2:
+        context.go('/favorites');
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
+      onTap: (value) => onItemTapped(context, value),
       elevation: 0,
+      currentIndex: getCurrentIndex(context),
       items: const [
         BottomNavigationBarItem(
           icon: Icon(Icons.home_max),
