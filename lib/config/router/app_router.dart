38e84de2cb7975ab0b729bcc1cef5544158d4e75
isaias-alt <cascolucasisaias@gmail.com>
2024-02-25 23:05:44 -0300

    refactor(config): add shell route

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -5,19 +5,34 @@ import 'package:wikicinema/presentation/views/views.dart';
 final appRouter = GoRouter(
   initialLocation: '/',
   routes: [
-    GoRoute(
-      path: '/',
-      name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(childView: HomeView()),
+    ShellRoute(
+      builder: (context, state, child) {
+        return HomeScreen(childView: child);
+      },
       routes: [
         GoRoute(
-          path: 'movie/:id',
-          name: MovieScreen.name,
+          path: '/',
+          name: HomeScreen.name,
           builder: (context, state) {
-            final movieId = state.pathParameters['id'] ?? 'no-id';
-            return MovieScreen(movieId: movieId);
+            return const HomeView();
           },
+          routes: [
+            GoRoute(
+              path: 'movie/:id',
+              name: MovieScreen.name,
+              builder: (context, state) {
+                final movieId = state.pathParameters['id'] ?? 'no-id';
+                return MovieScreen(movieId: movieId);
+              },
+            )
+          ],
         ),
+        GoRoute(
+          path: '/favorites',
+          builder: (context, state) {
+            return const FavoritesView();
+          },
+        )
       ],
     ),
   ],
