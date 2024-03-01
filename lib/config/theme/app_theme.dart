9b5d3709cda8f9e060d7c0aec7f40eaf6d122fb8
isaias-alt <cascolucasisaias@gmail.com>
2024-03-01 19:17:17 -0300

    feat(config): add dark theme

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   ThemeData getTheme() => ThemeData(
-        colorSchemeSeed: const Color(0xFF2862F5),
+        colorSchemeSeed: const Color(0xFF047B99),
+        brightness: Brightness.dark,
       );
 }
